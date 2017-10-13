1b961068d128bfeb3358b73b159758577e960d5f
Stefano Cecere <stefano.cecere@krur.com>
2017-10-13 09:42:04 +0200

    move MagicaVoxel into Tools

--- Assets/Plugins/MagicaVoxel/Scripts/Editor/MVEditorUtilities.cs
+++ Assets/Plugins/MagicaVoxel/Scripts/Editor/MVEditorUtilities.cs
@@ -3,45 +3,45 @@ using UnityEditor;
 using System.Collections;
 
 
-public static class MVEditorUtilities {
-	
-	public static Mesh CubeMesh()
-	{
-		return AssetDatabase.LoadAssetAtPath("Assets/MagicaVoxel/Meshes/cube.asset", typeof(Mesh)) as Mesh;
-	}
+public static class MVEditorUtilities
+{
 
-	public static Mesh QuadMesh()
-	{
-		return AssetDatabase.LoadAssetAtPath("Assets/MagicaVoxel/Meshes/quad.asset", typeof(Mesh)) as Mesh;
-	}
+    public static Mesh CubeMesh()
+    {
+        return AssetDatabase.LoadAssetAtPath("Assets/MagicaVoxel/Meshes/cube.asset", typeof(Mesh)) as Mesh;
+    }
 
-	[MenuItem("MagicaVoxel/Load")]
-	static void Load() {
-		string path = EditorUtility.OpenFilePanel(
-			"Open VOX model",
-			"Assets/MagicaVoxel/Vox",
-			"vox"
-		);
+    public static Mesh QuadMesh()
+    {
+        return AssetDatabase.LoadAssetAtPath("Assets/MagicaVoxel/Meshes/quad.asset", typeof(Mesh)) as Mesh;
+    }
 
-		if (path != null && path.Length > 0)
-		{
-			string alphaMaskPath = string.Empty;
-			if (EditorUtility.DisplayDialog("Question", "Do you want to load an alpha mask model?", "yes", "no"))
-			{
-				alphaMaskPath = EditorUtility.OpenFilePanel(
-					"Open VOX model for alpha mask",
-					"Assets/MagicaVoxel/Vox",
-					"vox"
-				);
-			}
+    [MenuItem("Tools/MagicaVoxel/Load")]
+    static void Load()
+    {
+        string path = EditorUtility.OpenFilePanel(
+            "Open VOX model",
+            "Assets/MagicaVoxel/Vox",
+            "vox"
+        );
 
-			GameObject go = new GameObject ();
-			go.name = System.IO.Path.GetFileNameWithoutExtension (path);
+        if (path != null && path.Length > 0) {
+            string alphaMaskPath = string.Empty;
+            if (EditorUtility.DisplayDialog("Question", "Do you want to load an alpha mask model?", "yes", "no")) {
+                alphaMaskPath = EditorUtility.OpenFilePanel(
+                    "Open VOX model for alpha mask",
+                    "Assets/MagicaVoxel/Vox",
+                    "vox"
+                );
+            }
 
-			MVVoxModel voxModel = go.AddComponent<MVVoxModel> ();
-			voxModel.ed_filePath = path;
-			voxModel.ed_alphaMaskFilePath = alphaMaskPath;
-			voxModel.LoadVOXFile (path, alphaMaskPath, voxModel.ed_importAsIndividualVoxels);
-		}
-	}
+            GameObject go = new GameObject();
+            go.name = System.IO.Path.GetFileNameWithoutExtension(path);
+
+            MVVoxModel voxModel = go.AddComponent<MVVoxModel>();
+            voxModel.ed_filePath = path;
+            voxModel.ed_alphaMaskFilePath = alphaMaskPath;
+            voxModel.LoadVOXFile(path, alphaMaskPath, voxModel.ed_importAsIndividualVoxels);
+        }
+    }
 }
