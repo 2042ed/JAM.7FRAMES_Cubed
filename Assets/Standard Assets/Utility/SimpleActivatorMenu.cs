409d6c00ecf62f314850df3ce2801d4f852c3254
Stefano Cecere <stefano.cecere@krur.com>
2020-08-31 17:16:39 +0200

    Unity 2020.1.3f1

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -5,9 +5,7 @@ namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
-        // An incredibly simple menu which, when given references
-        // to gameobjects in the scene
-        public GUIText camSwitchButton;
+
         public GameObject[] objects;
 
 
@@ -18,7 +16,6 @@ namespace UnityStandardAssets.Utility
         {
             // active object starts from first in array
             m_CurrentActiveObject = 0;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
 
 
@@ -26,13 +23,11 @@ namespace UnityStandardAssets.Utility
         {
             int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
 
-            for (int i = 0; i < objects.Length; i++)
-            {
+            for (int i = 0; i < objects.Length; i++) {
                 objects[i].SetActive(i == nextactiveobject);
             }
 
             m_CurrentActiveObject = nextactiveobject;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
     }
 }
